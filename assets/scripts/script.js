b5e2d954c8bb85398c71de4f462c81fd3cadb249
Chris Stevenson <chrislstevenson@gmail.com>
2023-12-12 05:15:39 -0500

    Restructured the program
    - added a container for all the questions in the html
    - organized the script file better
    - at this point we can navigate to the high scores and back
    - and we can start the quiz

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,64 +1,62 @@
 // Clicking the "View High Scores" element will go to the page
 
 // select by ID
-var viewHighScoresEl = document.getElementById("high-scores-link");
 var launchPageEl = document.getElementById("launch-page");
 var highScoresPageEl = document.getElementById("high-scores");
+var questionsContainerEl = document.getElementById("all-questions");
 
-// add event listener
+
+// TODO add countdown timer
+
+
+///////////////////////////////////////////////////////////////////////////////
+//                              Event Listeners                              //
+///////////////////////////////////////////////////////////////////////////////
+
+// view the high scores from the launch screen
+var viewHighScoresEl = document.getElementById("high-scores-link");
 viewHighScoresEl.addEventListener("click", function() {
-  // turn off the launch page display
+  // turn off the launch page display, turn on the high scores page
   launchPageEl.style.display = "none";
-  // turn on the high scores page
   highScoresPageEl.style.display = "block";
 });
 
-// now need a way to return to the launch page from the high scores page
+// return to the launch page from the high scores page
 var btnGoBackEl = document.getElementById("go-back");
-
 btnGoBackEl.addEventListener("click", function() {
   // turn off the high scores page and turn on the launch page
   launchPageEl.style.display = "block";
   highScoresPageEl.style.display = "none";
 });
 
-
-// TODO add countdown timer
-
-
-// Make "Start Quiz" button clickable to go to Q1
+// button to start the quiz
 var btnStartQuizEl = document.getElementById("btn-start");
-
-// retrieve an array of question lists and quiz responses
-var questionsEl = document.querySelectorAll(".question");
-
-// loop thru each question and get the responses
-var answerList;
-
 btnStartQuizEl.addEventListener("click", function() {
-  // turn off the launch page and turn on the Q1 page
+  // turn off the launch page
   launchPageEl.style.display = "none";
-  questionsEl[questionNumber].style.display = "block";
+
+  // turn on the section with the first question
+  // for now this is a place-holder, eventually it will be chosen programmatically
+  firstQuestionEl = document.getElementById("Q1")
+  firstQuestionEl.style.display = "block";
 });
 
-var questionNumber = 0;
-
-/*
- * TODO For each question, respond to user selection
- * If the user selects the correct answer, move to next window and append "Correct!"
- * If the user selects the wrong answer, subtract 10 sec from the timer, move to the next window and append "Wrong!"
- */
-
-// return list of possible answers (which are child elements of the ol)
-answerList = questionsEl[questionNumber].children[1];
-
+// set event listener for the div containing all the questions
+// when the user clicks on their choice of answer,
+// get the target by event delegation and proceed accordingly
 answerList.addEventListener("click", function(evt) {
   // retrieve the button that was selected
   var chosenAnswerEl = evt.target;
-  // hide the current question
-  questionsEl[questionNumber].style.display = "none";
-  // increment the question
-  questionNumber++;
+
+  /*
+   * Take action:
+   * - hide the current question
+   * - if the next question exists, display it
+   * - give feedback to the user on whether they got the previous Q wrong
+   * - if the next question doesn't exist, display the "finished" section
+   */
+
+  /*
   if (chosenAnswerEl.className == 'correct') {
     console.log("The choice was correct");
     questionsEl[questionNumber].children[2].textContent = "Correct!"
@@ -68,4 +66,5 @@ answerList.addEventListener("click", function(evt) {
     questionsEl[questionNumber].children[2].textContent = "Wrong!"
     questionsEl[questionNumber].style.display = "block";
   }
+   */
 });
